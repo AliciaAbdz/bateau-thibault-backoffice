dbe9bb373e6fb89cd87d7a273489aef5ff6bf63d
AliciaAbdz <alicia.abdz@gmail.com>
2026-03-02 11:42:15 +0100

    create product component, service & interface

---
+++ src/app/pages/details-produits/details-produits.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-details-produits',
+  imports: [],
+  templateUrl: './details-produits.component.html',
+  styleUrl: './details-produits.component.css'
+})
+export class DetailsProduitsComponent {
+
+}

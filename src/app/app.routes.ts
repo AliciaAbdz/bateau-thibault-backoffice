3ba0adc86c464bf1eade182ea1a789d125856af6
AliciaAbdz <alicia.abdz@gmail.com>
2026-03-02 11:42:28 +0100

    add product component to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,8 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
   },
+  {
+    path: 'details-produits',
+    loadComponent: () => import('./pages/details-produits/details-produits.component').then((m) => m.DetailsProduitsComponent )
+  }
 ];

52dd3e160208f126ddcca295b069772d553e4ace
Sivan-C <sivancozzo@gmail.com>
2026-03-13 11:45:09 +0100

    Mise en place de l'architecture du projet

---
+++ frontend/src/app/pages/products/validate-product-page/validate-product-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validate-product-page',
+  imports: [],
+  templateUrl: './validate-product-page.component.html',
+  styleUrl: './validate-product-page.component.css'
+})
+export class ValidateProductPageComponent {
+
+}

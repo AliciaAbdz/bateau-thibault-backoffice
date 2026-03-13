27eb4380107b4a5d85173416f37d5bc3c5d87880
AliciaAbdz <alicia.abdz@gmail.com>
2026-03-13 17:13:41 +0100

    create api client service & product service

--- frontend/src/app/services/client/client.service.ts
+++ frontend/src/app/services/client/client.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Product, User } from '../../models/models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8000/api/client';
+
+  constructor(private http:HttpClient) { }
+
+  getAllUsers() {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getAllProducts() {
+    return this.http.get<Product[]>(`${this.apiUrl}/products`);
+  }
 }
--- frontend/src/app/services/products/products.service.ts
+++ frontend/src/app/services/products/products.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Product } from '../../models/models';
+import { ClientService } from '../client/client.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  constructor(private client:ClientService) { }
+
+  addProducts(products:Product[]) {
+  }
+
+  modifyProducts(products:Product[]) {
+  }
+
+  deleteProducts(products:Product[]) {
+  }
 }

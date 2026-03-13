9d775b32a9609537ad15720ae1e43e23f8ecfdb4
AliciaAbdz <alicia.abdz@gmail.com>
2026-03-13 15:55:25 +0100

    install tailwind and update layouts components

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'slideDown': 'slideDown 0.2s ease',
+      },
+      keyframes: {
+        slideDown: {
+          'from': {
+            opacity: '0',
+            transform: 'translateY(-10px)',
+          },
+          'to': {
+            opacity: '1',
+            transform: 'translateY(0)',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}

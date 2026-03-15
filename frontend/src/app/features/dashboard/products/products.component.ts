8c99261d4a6c74de948c5afa31bf46574a2bfcec
Sivan-C <sivancozzo@gmail.com>
2026-03-15 15:09:39 +0100

    séparation et intègration des composant team-card, user-card dans dashboard

--- frontend/src/app/features/dashboard/products/products.component.ts
+++ frontend/src/app/features/dashboard/products/products.component.ts
@@ -7,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
-
+  products = [
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '5', edit: '', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '25', edit: '31', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '', edit: '', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '', edit: '', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '7', edit: '', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: '', edit: '5', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+    { nom: 'Hommard', prix: '45€', prixPromo: 'blabla', pourcentagePromo: 'blabla', edit: '', quantiteStock: 'blabla', nbArticlesVendus: 'blabla', commentaire: 'blabla' },
+  ];
 }

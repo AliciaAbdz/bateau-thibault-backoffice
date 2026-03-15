764673be4cfb744ed9e435e9dd7dbb6505e2d2d2
AliciaAbdz <alicia.abdz@gmail.com>
2026-03-15 22:30:17 +0100

    product specific treatment methods

--- frontend/src/app/services/products/products.service.ts
+++ frontend/src/app/services/products/products.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { Product } from '../../models/models';
+import { RetailArticle, RetailArticleDisplay } from '../../models/models';
 import { ClientService } from '../client/client.service';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,36 @@ export class ProductsService {
 
   constructor(private client:ClientService) { }
 
-  addProducts(products:Product[]) {
+  getArticles(id:number) : Observable<RetailArticleDisplay[]> {
+    return this.client.getAllRetailArticles(id).pipe(
+      map(articles => articles.map(article => this.toDisplay(article)))
+    );
   }
 
-  modifyProducts(products:Product[]) {
+  getRetailerCategory(articles : RetailArticleDisplay[]) : string[]{
+    return [...new Set(articles.map(article => article.category))];
   }
 
-  deleteProducts(products:Product[]) {
+  private toDisplay(article: RetailArticle): RetailArticleDisplay {
+    const discountPrice = article.price * (1 - article.discount_percent / 100);
+    return {
+      id: article.id,
+      name: article.name,
+      category: article.category,
+      price: article.price + ' €',
+      discount_price: discountPrice.toFixed(2) + ' €',
+      discount_percent: article.discount_percent + ' %',
+      stock: String(article.stock),
+      sales: String(article.sales),
+      comment: article.comment,
+    };
+  }
+  addProducts(products:RetailArticle[]) {
+  }
+
+  modifyProducts(products:RetailArticle[]) {
+  }
+
+  deleteProducts(products:RetailArticle[]) {
   }
 }
